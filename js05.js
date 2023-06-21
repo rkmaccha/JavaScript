068c09c7788cffa186276d9bf371019311cb0df6
Ram Maccha <50056184+rkmaccha@users.noreply.github.com>
2023-06-21 21:28:42 +0100

    Update js05.js

--- js05.js
+++ js05.js
@@ -13,6 +13,17 @@ let hasMasters = true;
 // BigInt
 // Symbol
 
+// console.log(typeof age);
+// console.log(typeof hasMasters);
+
 
 // Convert number to string or vice-versa
+//age_string = age +"";
+age_string = String(age)
+// console.log(typeof age_string);
 
+let cash = "55";
+console.log(typeof cash);
+// let cash_int = +cash;
+let cash_int = Number(cash)
+console.log( typeof cash_int);
